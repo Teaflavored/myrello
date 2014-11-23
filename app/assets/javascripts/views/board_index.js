0e8097ddfbc66f860665a04b7d89a1321a595a7c
Auster Chen <Auster.s.chen@gmail.com>
2014-11-23 14:05:47 -0800

    refactored boards index

--- app/assets/javascripts/views/board_index.js
+++ app/assets/javascripts/views/board_index.js
@@ -1,52 +1,55 @@
 Trello.Views.BoardIndex = Backbone.CompositeView.extend({
   template: JST["boards/index"],
+
   attributes: {
     "id":"index"
   },
 
   events: {
+    //can click on item to show board or click on button to display new form
+
     "click button.new-form": "showNewForm",
     "click li.board-index-item": "showBoard",
   },
 
   initialize: function(options){
     //need to listen to whenever an element is added to collection
-    //then add a subview
-    this.listenTo(this.collection, "add", this.addView)
 
-    this.boardsSelector = "ul.boards"
+    this.listenTo(this.collection, "add", this.addView)
+    this.boardListSelector = "ul.boards"
+
     //need to attach each of the views if they were already fetched
+
     this.collection.each(function(board){
-      this.addView(board)
+      this.addView(board);
     }.bind(this))
   },
 
   showBoard: function(event){
-    var boardId = $(event.currentTarget).data("id")
-    Backbone.history.navigate("#/boards/" + boardId, { trigger: true })
+    //grab the id from the board index item and navigate there
+
+    var boardId = $(event.currentTarget).data("id");
+    Backbone.history.navigate("#/boards/" + boardId, { trigger: true });
   },
 
   showNewForm: function(event){
-    event.preventDefault()
-    this.$newForm = this.$("div.new-form")
-    this.$newForm.focus();
-    var newBoardView = new Trello.Views.BoardNew()
-    this.$newForm.html(newBoardView.render().$el)
+    event.preventDefault();
+    this.$("div.new-form").focus();
+    var newBoardView = new Trello.Views.BoardNew();
+    this.$("div.new-form").html(newBoardView.render().$el);
   },
 
   addView: function(board){
     var indexItemView = new Trello.Views.BoardIndexItem({
       model: board
     })
-    this.addSubviewPre(this.boardsSelector, indexItemView)
+    this.addSubviewPre(this.boardListSelector, indexItemView);
   },
 
   render: function(){
-    var renderedContent = this.template()
-    this.$el.html(renderedContent)
+    var renderedContent = this.template();
+    this.$el.html(renderedContent);
     this.attachSubviews();
     return this;
-  },
-
-
+  }
 })
