b4c2fad0ecb18ff35c8432389a6f56b0e6042b8f
Auster Chen <Auster.s.chen@gmail.com>
2014-11-22 23:11:19 -0800

    fixed draggable cards, made sortabl

--- app/assets/javascripts/views/card_item.js
+++ app/assets/javascripts/views/card_item.js
@@ -14,11 +14,6 @@ Trello.Views.CardItemView = Backbone.View.extend({
     })
 
     this.$el.html(renderedContent)
-    this.$el.draggable({
-      drop: function(event, ui){
-        alert('test')
-      }
-    })
     return this;
   }
 })
